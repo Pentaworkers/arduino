e486a80a79425351ab2a179aac19aeac7952e998
Lee Zhuohua <lzhblack@126.com>
2017-03-31 22:11:10 +0800

    bluetooth to drive e_m

---
+++ arduino/run_bt.ino
@@ -0,0 +1,38 @@
+//蓝牙驱动电机
+int run1 = 9;
+int run2 = 10;
+//int runSpeed = 11;
+int tmp;
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(run1,OUTPUT);
+    pinMode(run2,OUTPUT);
+//    pinMode(runSpeed,OUTPUT);
+}
+void loop()
+{
+   tmp = Serial.read();
+   if(tmp == '1'){
+    digitalWrite(run1,HIGH);
+    digitalWrite(run2,LOW);
+    analogWrite(runSpeed,100);
+    Serial.println("go");
+    delay(2000);
+   }
+    if(tmp == '2'){
+    digitalWrite(run1,HIGH);
+    digitalWrite(run2,LOW);
+    analogWrite(runSpeed,200);
+    Serial.println("go");
+    delay(2000);
+   }
+   if(tmp == '0'){
+    digitalWrite(run1,HIGH);
+    digitalWrite(run2,HIGH);
+    Serial.println("stop");
+    delay(2000);
+   }
+        
+}
